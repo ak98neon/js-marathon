623d3232f892935ec0ad188d427f536afae9465a
ak98neon <ak98nneon@gmail.com>
2020-08-22 01:06:25 +0300

    HM-3: Make a mini browser pokemon game :)

--- main.js
+++ main.js
@@ -1,23 +1,96 @@
-let phoneNumber = prompt("Please input phone number: ");
-alert('Your input: ' + phoneNumber + '\n' + 'Formatted: ' + formattedPhone(phoneNumber));
+class Character {
+    constructor(name, hp, damageHp) {
+        this.name = name;
+        this.hp = hp;
+        this.damageHp = damageHp;
+    }
 
-function formattedPhone(phone) {
-    if (isPhoneLengthNotValid(phone)) {
-        return "Number length is wrong!";
-    } else {
-        let formattedPhoneNumber = '';
+    initHp() {
+        throw new Error('You have to implement the method initHp!');
+    }
 
-        formattedPhoneNumber += phone.substring(0, 2) + ' ';
-        formattedPhoneNumber += '(' + phone.substring(2, 5) + ') ';
-        formattedPhoneNumber += phone.substring(5, 8) + '-';
-        formattedPhoneNumber += phone.substring(8, 10) + '-';
-        formattedPhoneNumber += phone.substring(10, 12);
-        return formattedPhoneNumber;
+    hit(damage) {
+        if (this.damageHp - damage < 0) {
+            this.damageHp = 0;
+            alert('You are lose ' + this.name);
+        } else {
+            this.damageHp -= damage;
+        }
+
+        this.initHp();
     }
 }
 
-function isPhoneLengthNotValid(phone) {
-    return phone.length < 12 || phone.length > 12;
+class Player extends Character {
+    isUsedPowerPush = false;
+    elHP = document.getElementById('health-character');
+    elProgressBar = document.getElementById('progressbar-character');
+
+    constructor(hp, damageHp) {
+        super('Player', hp, damageHp);
+    }
+
+    initHp() {
+        this.elHP.innerText = this.damageHp + ' / ' + this.hp;
+        this.elProgressBar.style.width = this.damageHp + '%';
+    }
+
+    powerPush() {
+        if (!this.isUsedPowerPush) {
+            this.isUsedPowerPush = true;
+            return 50;
+        } else {
+            alert('You have used power push already!');
+            return 0;
+        }
+    }
 }
 
-console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
+class Enemy extends Character {
+    elHP = document.getElementById('health-enemy');
+    elProgressBar = document.getElementById('progressbar-enemy');
+
+    constructor(hp, damageHp) {
+        super('Enemy', hp, damageHp);
+    }
+
+
+    initHp() {
+        this.elHP.innerText = this.damageHp + ' / ' + this.hp;
+        this.elProgressBar.style.width = this.damageHp + '%';
+    }
+}
+
+let player = new Player(100, 100);
+let enemy = new Enemy(100, 100);
+
+function initGame() {
+    console.log("Start Game!");
+
+    player.initHp();
+    enemy.initHp();
+}
+
+function changeHp(damage, character) {
+    character.hit(damage);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+let $btn = document.getElementById('btn-kick');
+
+$btn.addEventListener('click', function () {
+    console.log('kick');
+    changeHp(random(20), player);
+    changeHp(random(20), enemy);
+});
+
+let $btnPowerPush = document.getElementById('btn-power-push');
+$btnPowerPush.addEventListener('click', function () {
+    console.log('power push to enemy');
+    changeHp(player.powerPush(), enemy);
+});
+
+initGame();
